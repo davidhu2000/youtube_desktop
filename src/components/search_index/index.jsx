6bc48dc3b56427f9020f3685e777028f0e6aa932
David Hu <davidhu314@gmail.com>
2017-03-08 12:37:16 -0800

    set up basic search quering to youtube

--- src/components/search_index/index.jsx
+++ src/components/search_index/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import YT_API_KEY from '../../../config/api_key';
 
 import { SearchBar } from '../common';
 
@@ -9,6 +10,32 @@ import { receiveQuery } from '../../actions/query_actions';
 class SearchIndex extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  _fetchResult() {
+    let query = this.props.query;
+
+    if(query !== null) {
+      console.log('not null');
+      let base_url = `https://www.googleapis.com/youtube/v3/search`;
+      let part ='snippet';
+      let type = 'video';
+
+      let full_url = `${base_url}?part=${part}&q=${query}&type=${type}&key=${YT_API_KEY.publicDataKey}`;
+
+      fetch(full_url);
+    }
+
+  }
+
+  componentDidMount() {
+    this._fetchResult();
+  }
+
+  componentWillReceiveProps(newProps) {
+    if(this.props.query !== newProps.query) {
+      this._fetchResult();
+    }
   }
 
   render() {
