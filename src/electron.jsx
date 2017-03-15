068065318ed8f82612b235764bae6f0ea7cc3afd
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-03-14 18:10:07 -0700

    Add trending components based on existing search_index components

--- src/electron.jsx
+++ src/electron.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { trendingVideos } from './actions/search_result_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
@@ -12,5 +10,4 @@ document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root store={ store } />, root);
 
   window.store = store;
-  window.trendingVideos = trendingVideos;
 });
