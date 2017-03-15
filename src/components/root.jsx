0a979cf2235c4b6d75946861755579e68fd1e623
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-03-15 14:07:00 -0700

    Fix pull request comments

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -4,14 +4,14 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from './app';
 import SearchIndex from './search_index';
-import TrendingIndexContainer from './trending/trending_index_container';
+import TrendingIndex from './trending';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
     <Router history={ hashHistory }>
       <Route path='/' component={ App }>
         <Route path='/search' component={ SearchIndex } />
-        <Route path='/trending' component={ TrendingIndexContainer } />
+        <Route path='/trending' component={ TrendingIndex } />
       </Route>
     </Router>
   </Provider>
