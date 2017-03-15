7d39c3a3eb73dfe85aeacd9236e6262d4f6a48cb
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-03-14 17:50:46 -0700

    Add trending videos API built on exiting search result react dataflow

--- src/electron.jsx
+++ src/electron.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
+import { trendingVideos } from './actions/search_result_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
@@ -10,4 +12,5 @@ document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root store={ store } />, root);
 
   window.store = store;
+  window.trendingVideos = trendingVideos;
 });
