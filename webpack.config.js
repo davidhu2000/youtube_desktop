6eaae272b52a33b5abc5cb5f6a038fd61af07dd3
David Hu <davidhu314@gmail.com>
2017-03-20 14:22:40 -0700

    webpack can create multiple js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,61 @@
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  context: __dirname,
-  entry: "./src/electron.jsx",
-  output: {
-    path: path.join(__dirname, 'app', 'javascripts'),
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
+module.exports = env => {
+  const isProduction = plugin => env.prod ? plugin : undefined;
+  const removeEmpty = array => array.filter(p => !!p);
+
+  return {
+    context: __dirname,
+    entry: {
+      app: path.join(__dirname, './src/electron.jsx'),
+      vendor: ['react', 'react-dom', 'react-router']
+    },
+    output: {
+      path: path.join(__dirname, 'app', 'javascripts'),
+      filename: '[name].[hash].js',
+    },
+    module: {
+      loaders: [
+        {
+          test: [/\.jsx?$/, /\.js?$/],
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react'],
+            cacheDirectory: true
+          }
         }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*' ]
+      ]
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.js', '.jsx', '*' ]
+    },
+    plugins: removeEmpty([
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: Infinity,
+        filename: '[name].[hash].js'
+      }),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'template.html'),
+        filename: '../../index.html',
+        inject: 'body'
+      }),
+      isProduction(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          'screw_ie8': true,
+          'warnings': false,
+          'unused': true,
+          'dead_code': true
+        },
+        output: {
+          comments: false
+        },
+        sourceMap: false
+      }))
+    ])
   }
 };
