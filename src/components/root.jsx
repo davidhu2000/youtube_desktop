edeb4dd1e0c391c92495feeb022c35f8a93ca71d
David Hu <davidhu314@gmail.com>
2017-03-20 16:10:09 -0700

    add routes file, coding splitting functional

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-
-// main app components
-import App from './app';
-import SearchIndex from './search_index';
-import TrendingIndex from './trending';
-
+import routes from './routes';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
-    <Router history={ hashHistory }>
-      <Route path='/' component={ App }>
-        <Route path='/search' component={ SearchIndex } />
-        <Route path='/trending' component={ TrendingIndex } />
-      </Route>
-    </Router>
+    <Router history={ hashHistory } routes={routes} />
   </Provider>
 );
 
