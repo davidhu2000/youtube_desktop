3c4e72b5534c4fe66a2a106ee2c1be1eb21f7be7
Carson Judge <cjudge1337@gmail.com>
2017-03-21 17:15:44 -0700

    Store vids in local state

--- src/components/player/related.jsx
+++ src/components/player/related.jsx
@@ -3,18 +3,29 @@ import YT_API_KEY from '../../../config/api_key';
 
 class Related extends React.Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = { vids: [] };
+  }
+
   componentDidMount() {
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&relatedToVideoId=${this.props.videoId}&key=${YT_API_KEY.publicDataKey}`)
-    .then(response => response.json())
-    .then(responseJson => {
-      console.log(responseJson);
-    })
-    .catch(error => {
-      console.error(error);
-    })
+    this._fetchRelated();
+  }
+
+  _fetchRelated() {
+    return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&relatedToVideoId=${this.props.videoId}&key=${YT_API_KEY.publicDataKey}`)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ vids: responseJson.items});
+      })
+      .catch(error => {
+        console.error(error);
+      })
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
         Related
