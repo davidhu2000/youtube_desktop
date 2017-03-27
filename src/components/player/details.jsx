91516b1b00b3fa556b3b41b1d9821860d6a813d9
Carson Judge <cjudge1337@gmail.com>
2017-03-27 16:13:00 -0700

    Fetch video details successful

--- src/components/player/details.jsx
+++ src/components/player/details.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 class Details extends React.Component {
 
+  constuctor(props) {
+    super(props);
+
+    this.state = { details: [] };
+  }
+
+  _fetchDetails() {
+    return fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${this.props.videoId}&key=${YT_API_KEY.publicDataKey}`)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({  });
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
   render() {
     return (
       <div>
