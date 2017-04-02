f66b8d9f7efb750f56b42de59caa58492c075702
David Hu <davidhu314@gmail.com>
2017-04-02 16:50:19 -0700

    add indexRoute for home

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import routes from './routes';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
