320c54f20e62d0578c718421ca12bf1b256a539b
David Hu <davidhu314@gmail.com>
2017-04-02 19:40:57 -0700

    add VideoList common component, refactored search_index and trending_index

--- src/components/trending/trending_index.jsx
+++ src/components/trending/trending_index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router';
-import { VideoSearchItem, SmlVideoSearchItem } from '../common';
+import { VideoList } from '../common';
 
 class TrendingIndex extends React.Component {
   constructor(props) {
@@ -14,38 +14,10 @@ class TrendingIndex extends React.Component {
     }
   }
 
-  addSearchResults() {
-    if (this.props.trendingVideos) {
-      let vids = this.props.trendingVideos;
-      return vids.map(vid => <VideoSearchItem key={vid.etag} vid={vid} />);
-    }
-  }
-
-  addSmlSearchResults() {
-    if (this.props.trendingVideos) {
-      let vids = this.props.trendingVideos;
-      return vids.map(vid => <SmlVideoSearchItem key={vid.etag} vid={vid} />);
-    }
-  }
-
-  addSearchVolume() {
-    if (this.props.trendingVideos) {
-      let volume = Object.keys(this.props.trendingVideos).length;
-      return <p>About {volume} results</p>;
-    }
-  }
 
   render() {
     return (
-      <div className="search-index">
-        <div className="search-index-container">
-          <div className="search-index-container-top">
-            {this.addSearchVolume()}
-          </div>
-        {this.addSearchResults()}
-        {this.addSmlSearchResults()}
-        </div>
-      </div>
+      <VideoList videos={this.props.trendingVideos} />
     );
   }
 }
