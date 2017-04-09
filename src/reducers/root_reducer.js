f4f2ab3fb7487f0a3fb6c844ce44a5058c12ed5b
David Hu <davidhu314@gmail.com>
2017-04-08 18:56:23 -0700

    add redux cycle for user info

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -3,12 +3,14 @@ import queryReducer from './query_reducer';
 import searchResultReducer from './search_result_reducer';
 import trendingReducer from './trending_reducer';
 import channelsReducer from './channels_reducer';
+import userReducer from './user_reducer';
 
 const rootReducer = combineReducers({
   channels: channelsReducer,
   query: queryReducer,
   searchResult: searchResultReducer,
   trending: trendingReducer,
+  user: userReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/user_reducer.js
@@ -0,0 +1,17 @@
+import { merge } from 'lodash';
+import {
+  RECEIVE_USER } from "../actions/oauth_actions";
+
+let _defaultState = {};
+
+const userReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_USER:
+      return action.user;
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
