a27f24ee0909c63a563afece419e8f638abc2558
David Hu <davidhu314@gmail.com>
2017-04-12 16:42:14 -0700

    can open up new window and render video player

--- src/renderer/video_page.js
+++ src/renderer/video_page.js
@@ -3,22 +3,23 @@ const { BrowserWindow } = electron.remote;
 const path = require('path');
 const url = require('url');
 
-
 const videoWindow = videoId => {
   let videoWin;
 
   videoWin = new BrowserWindow({
     minWidth: 230,
     minHeight: 130,
-    width: 400,
-    height: 300
+    width: 1000,
+    height: 800
   });
 
+  videoWin.webContents.openDevTools();
+
   videoWin.loadURL(url.format({
-    pathname: path.join(__dirname, `index.html?videoId=${videoId}`),
+    pathname: path.join(__dirname, 'src', 'renderer', `index.html`),
     protocol: 'file:',
     slashes: true
-  }));
+  }) + `?videoId=${videoId}`);
 
   videoWin.on('closed', () => {
     videoWin = null;
