582c8d5d3f1a964603a319081f00a8cf6a2b5177
Raymond Lee <raylee0525@gmail.com>
2017-04-12 16:34:19 -0700

    Implement banners, CSS styling

--- src/components/channel/index.jsx
+++ src/components/channel/index.jsx
@@ -10,9 +10,32 @@ class Channel extends React.Component {
   render() {
     return (
       <div className='channel'>
-        <div>Cover photo</div>
-        <div>subscribe </div>
-        <div>navbar</div>
+        <div className="banner-container">
+          <img id="banner" src="./app/assets/banner1.png"/>
+        </div>
+        <div className="channel-banner">
+          <div className="channel-banner-left">
+            <img id="channel-profile" src="./app/assets/banner.png"/>
+            <div id="channel-details">
+              <span>NAME</span>
+              <span>SUBSCRIBERS</span>
+            </div>
+          </div>
+          <div className="channel-banner-right">
+            <button>SUBSCRIBE</button>
+          </div>
+        </div>
+        <div id="banner-bar"></div>
+        <div id="channel-navbar">
+          <ul>
+            <li>HOME</li>
+            <li>VIDEOS</li>
+            <li>PLAYLISTS</li>
+            <li>CHANNELS</li>
+            <li>DISCUSSION</li>
+            <li>ABOUT</li>
+          </ul>
+        </div>
         <div>
           <div>Main Video</div>
           <div>Main Video Description</div>
