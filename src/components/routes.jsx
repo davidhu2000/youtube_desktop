d1bdc6f96f0272185787ec62f1cacff4b6d97fcd
David Hu <davidhu314@gmail.com>
2017-04-13 23:09:02 -0700

    add subscription route

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -11,40 +11,30 @@ const loadRoute = callback => {
 }
 
 const routes = (
-  <Route path='/' component={ App } >
-    <IndexRoute
-       getComponent={ (location, cb) =>  {
-         System.import('./home').then(loadRoute(cb)).catch(errorLoading)
-       } }
-    />
+  <Route path='/' component={App}>
+    <IndexRoute getComponent={(location, cb) => {
+      System.import ('./home').then(loadRoute(cb)).catch(errorLoading)
+    }}/>
 
-    <Route
-      path='search'
-      getComponent={ (location, cb) =>  {
-        System.import('./search_index').then(loadRoute(cb)).catch(errorLoading)
-      } }
-    />
+    <Route path='search' getComponent={(location, cb) => {
+      System.import ('./search_index').then(loadRoute(cb)).catch(errorLoading)
+    }}/>
 
-    <Route
-      path='trending'
-      getComponent={ (location, cb) =>  {
-        System.import('./trending').then(loadRoute(cb)).catch(errorLoading)
-      } }
-    />
+    <Route path='trending' getComponent={(location, cb) => {
+      System.import ('./trending').then(loadRoute(cb)).catch(errorLoading)
+    }}/>
 
-    <Route
-      path='video/:videoId'
-      getComponent={ (location, cb) =>  {
-        System.import('./player').then(loadRoute(cb)).catch(errorLoading)
-      } }
-    />
+    <Route path='video/:videoId' getComponent={(location, cb) => {
+      System.import ('./player').then(loadRoute(cb)).catch(errorLoading)
+    }}/>
 
-    <Route
-      path='home'
-      getComponent={ (location, cb) =>  {
-        System.import('./home').then(loadRoute(cb)).catch(errorLoading)
-      } }
-    />
+    <Route path='subscriptions' getComponent={(location, cb) => {
+      System.import ('./subscriptions').then(loadRoute(cb)).catch(errorLoading)
+    }}/>
+
+    <Route path='home' getComponent={(location, cb) => {
+      System.import ('./home').then(loadRoute(cb)).catch(errorLoading)
+    }}/>
 
   </Route>
 );
