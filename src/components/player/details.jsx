d8f6c72162cb84b5899aff0868c4361ff362a4f5
Alex Sherman <asherman.ca@gmail.com>
2017-04-19 14:40:08 -0700

    detail section elements added and styled

--- src/components/player/details.jsx
+++ src/components/player/details.jsx
@@ -16,7 +16,7 @@ class Details extends React.Component {
 
   componentDidMount() {
     fetchDetails(this.props.videoId, this);
-  }
+    }
 
   componentWillReceiveProps(newProps) {
     if(newProps.videoId !== this.props.videoId) {
@@ -36,40 +36,62 @@ class Details extends React.Component {
     if (!this.state.details.snippet) {
       return null;
     }
-
+    console.log(this.state);
     const { title, channelTitle, publishedAt } = this.state.details.snippet;
     const { viewCount, likeCount, dislikeCount } = this.state.details.statistics;
 
     return (
       <div className="details-container">
-        <span className="title">{title}</span>
-        <a href="" className="channel-name">{channelTitle}</a>
-        <button type="button" className="sub-button">
-          <i className="material-icons">play_arrow</i>
-          <span>Subscribe</span>
-        </button>
-        <span>{Number(this.state.subs).toLocaleString()}</span>
-        <span className="total-views">{Number(viewCount).toLocaleString()}</span>
-        <button type="button" className="add-button">
-          <i className="material-icons">add</i>
-          <span>Add to</span>
-        </button>
-        <button type="button" className="share-button">
-          <i className="material-icons">share</i>
-          <span>Share</span>
-        </button>
-        <button type="button" className="like-button">
-          <i className="material-icons">thumb_up</i>
-          <span>{Number(likeCount).toLocaleString()}</span>
-        </button>
-        <button type="button" className="dislike-button">
-          <i className="material-icons">thumb_down</i>
-          <span>{Number(dislikeCount).toLocaleString()}</span>
-        </button>
-        <h5 className="details-date">
-          Published on {parseDate(publishedAt)}
-        </h5>
-        {this.addDescription()}
+        <div className="details-upper-container">
+          <h1 className="title">{title}</h1>
+          <div className="details-stats-bar">
+            <div className="details-stats-bar-left">
+              <a href="" className="channel-name">{channelTitle}</a>
+              <div className="button-span">
+              <button type="button" className="sub-button">
+                <img className='white-burger' src="./app/assets/whiteburger.png"/>
+                <span>Subscribe</span>
+              </button>
+              <span className="sub-span">{Number(this.state.subs).toLocaleString()}</span>
+              </div>
+            </div>
+            <div className="details-stats-bar-right">
+              <span className="total-views">{Number(viewCount).toLocaleString()} views</span>
+            </div>
+          </div>
+          <div className="details-action-bar">
+            <div className="details-action-bar-left">
+              <button type="button" className="add-button">
+                <i className="material-icons">add</i>
+                <span>Add to</span>
+              </button>
+              <button type="button" className="share-button">
+                <i className="material-icons">share</i>
+                <span>Share</span>
+              </button>
+            </div>
+            <div className="details-action-bar-right">
+              <button type="button" className="like-button">
+                <i className="material-icons">thumb_up</i>
+                <span>{Number(likeCount).toLocaleString()}</span>
+              </button>
+              <button type="button" className="dislike-button">
+                <i className="material-icons">thumb_down</i>
+                <span>{Number(dislikeCount).toLocaleString()}</span>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="details-lower-container">
+          <h3 className="details-date">
+            Published on {parseDate(publishedAt)}
+          </h3>
+          <p>
+          {this.addDescription()}
+          </p>
+        </div>
+
       </div>
     );
   }
