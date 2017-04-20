8bbb1a6c45843bebfb24bf5c05866b5481d716c0
David Hu <davidhu314@gmail.com>
2017-04-19 17:55:08 -0700

    update styling of show more button on description

--- src/components/player/details_lower.jsx
+++ src/components/player/details_lower.jsx
@@ -24,24 +24,15 @@ class DetailsLower extends React.Component {
   addDescription() {
     if (this.props.description){
       let descript;
-      let buttonVal;
       if(this.state.showAllDecription) {
         descript = this.props.description.split('\n');
-        buttonVal = 'Show less';
       } else {
         descript = [this.props.description.slice(0,200)];
-        buttonVal = 'Show more';
       }
 
       return (
         <p className="description">
           { this.parseDescription(descript) }
-          <hr />
-          <button 
-            className='details-description-button'
-            onClick={ () => this.setState({ showAllDecription: !this.state.showAllDecription }) }>
-            { buttonVal }
-          </button>
         </p>
       );
     }
@@ -53,8 +44,13 @@ class DetailsLower extends React.Component {
         <h3 className="details-date">
           Published on {parseDate(this.props.publishedAt)}
         </h3>
-        <div>
+        <div className="details-lower-description">
           {this.addDescription()}
+          <button 
+            className='details-description-button'
+            onClick={ () => this.setState({ showAllDecription: !this.state.showAllDecription }) }>
+            { this.state.showAllDecription ? 'Show less' : 'Show more' }
+          </button>
         </div>
       </div>
     );
