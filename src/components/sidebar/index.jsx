5a9fd9779be139242375ce5b4a7fb763c95f9d10
Raymond Lee <raylee0525@gmail.com>
2017-04-22 15:58:17 -0700

    more styling, localstorage - mychannelid

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import Sidebar from './sidebar';
+import { fetchChannelId }   from '../../actions/youtube_video_actions';
 
 const mapStateToProps = ({ user, subscriptions, setting }) => ({
   loggedIn: Boolean(user),
