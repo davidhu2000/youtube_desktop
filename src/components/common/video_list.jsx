2ea3f96489dd06709d68b78423586f449315ab1a
David Hu <davidhu314@gmail.com>
2017-04-23 14:18:28 -0700

    update video search item to video list item

--- src/components/common/video_list.jsx
+++ src/components/common/video_list.jsx
@@ -1,7 +1,7 @@
 import React                from 'react';
 import PropTypes            from 'prop-types';
 import { formatNumber }     from '../../helpers';
-import { VideoSearchItem }  from '../common';
+import { VideoListItem }  from '../common';
 
 class VideoList extends React.Component {
   constructor(props) {
