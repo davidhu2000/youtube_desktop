021c724d71c20a925ec885389834d7f0cdae2514
Carson Judge <cjudge1337@gmail.com>
2017-04-26 19:48:45 -0700

    Add event methods for new comment

--- src/components/player/new_comment.jsx
+++ src/components/player/new_comment.jsx
@@ -10,13 +10,29 @@ class NewComment extends React.Component {
     this.state = {
       body: ""
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({ body: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    
   }
 
   render() {
     return (
-      <form className="new-comment-form">
-        <input type="text" className="" />
-        <input type="submit" className="" />
+      <form onSubmit={this.handleSubmit} className="new-comment-form">
+        <input type="text"
+               placeholder="Add a public comment..."
+               onChange={this.handleChange}
+               className="" />
+        <input type="submit" value="Comment" className="" />
       </form>
     );
   }
