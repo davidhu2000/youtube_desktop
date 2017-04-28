f5c3ae375b2171492d480722c066229512ca0c13
David Hu <davidhu314@gmail.com>
2017-04-28 14:37:15 -0700

    add progress bar component

---
+++ src/components/common/progress_bar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProgressBar extends React.Component {
+   constructor(props) {
+     super(props);
+  }
+
+  render() {
+    return (
+      <div id='progress-bar'></div>
+    );
+  }
+}
+
+export { ProgressBar };
