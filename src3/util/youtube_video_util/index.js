feefa3f4b4b85dce2afffe249edc908364151461
Raymond Lee <raylee0525@gmail.com>
2017-04-29 16:31:30 -0700

    update playlist

--- src3/util/youtube_video_util/index.js
+++
@@ -1,6 +0,0 @@
-export * from './channel_util.js';
-export * from './recommended_util.js';
-export * from './subscription_util.js';
-export * from './trending_util.js';
-export * from './video_util.js';
-export * from './playlist_util.js';
