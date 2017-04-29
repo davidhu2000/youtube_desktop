d41770b51974a342c78b215339165667bea800af
Raymond Lee <raylee0525@gmail.com>
2017-04-29 15:01:26 -0700

    update home structure

---
+++ src/modules/home/components/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Home from './home';
+import { receiveSetting } from 'common/setting/actions';
+
+import {
+  fetchTrending,
+  fetchCategories,
+  fetchChannelInfo,
+  fetchChannelVideos,
+  fetchRecommendedVideos } from '../../actions/youtube_video_actions'; // TODO: Update!
+
+const mapStateToProps = ({ trending, channels, user, recommended, setting }) => ({
+  trending,
+  channels,
+  loggedIn: Boolean(user),
+  recommended,
+  setting,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchTrending: () => dispatch(fetchTrending()),
+  fetchChannelInfo: id => dispatch(fetchChannelInfo(id)),
+  fetchChannelVideos: id => dispatch(fetchChannelVideos(id)),
+  fetchCategories: () => dispatch(fetchCategories()),
+  fetchRecommendedVideos: () => dispatch(fetchRecommendedVideos()),
+  receiveSetting: setting => dispatch(receiveSetting(setting))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
--- src3/components/home/index.jsx
+++ src3/components/home/index.jsx
@@ -1,34 +1,34 @@
-import React       from 'react';
-import { connect } from 'react-redux';
-import Home        from './home.jsx';
-
-import { receiveSetting } from 'actions/setting_actions';
-
-import {
-  fetchTrending,
-  fetchCategories,
-  fetchChannelInfo,
-  fetchChannelVideos,
-  fetchRecommendedVideos } from '../../actions/youtube_video_actions';
-
-const mapStateToProps = ({ trending, channels, user, recommended, setting }) => ({
-  trending,
-  channels,
-  loggedIn: Boolean(user),
-  recommended,
-  setting,
-});
-
-const mapDispatchToProps = dispatch => ({
-  fetchTrending: () => dispatch(fetchTrending()),
-  fetchChannelInfo: id => dispatch(fetchChannelInfo(id)),
-  fetchChannelVideos: id => dispatch(fetchChannelVideos(id)),
-  fetchCategories: () => dispatch(fetchCategories()),
-  fetchRecommendedVideos: () => dispatch(fetchRecommendedVideos()),
-  receiveSetting: setting => dispatch(receiveSetting(setting))
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+// import React       from 'react';
+// import { connect } from 'react-redux';
+// import Home        from './home.jsx';
+//
+// import { receiveSetting } from 'actions/setting_actions';
+//
+// import {
+//   fetchTrending,
+//   fetchCategories,
+//   fetchChannelInfo,
+//   fetchChannelVideos,
+//   fetchRecommendedVideos } from '../../actions/youtube_video_actions';
+//
+// const mapStateToProps = ({ trending, channels, user, recommended, setting }) => ({
+//   trending,
+//   channels,
+//   loggedIn: Boolean(user),
+//   recommended,
+//   setting,
+// });
+//
+// const mapDispatchToProps = dispatch => ({
+//   fetchTrending: () => dispatch(fetchTrending()),
+//   fetchChannelInfo: id => dispatch(fetchChannelInfo(id)),
+//   fetchChannelVideos: id => dispatch(fetchChannelVideos(id)),
+//   fetchCategories: () => dispatch(fetchCategories()),
+//   fetchRecommendedVideos: () => dispatch(fetchRecommendedVideos()),
+//   receiveSetting: setting => dispatch(receiveSetting(setting))
+// });
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(Home);
