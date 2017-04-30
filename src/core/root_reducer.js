3c7aa15279402a3677e2926166b30006869c43b6
Raymond Lee <raylee0525@gmail.com>
2017-04-29 21:11:25 -0700

    re order root reducer

--- src/core/root_reducer.js
+++ src/core/root_reducer.js
@@ -1,28 +1,30 @@
 import { combineReducers } from 'redux';
-import homeChannelsReducer from 'modules/home/reducer';
-import channelDetailsReducer from 'modules/channel/reducer';
+
 import queryReducer from 'common/query/reducer';
+import settingReducer from 'common/setting/reducer';
+
+import channelDetailsReducer from 'modules/channel/reducer';
+import homeChannelsReducer from 'modules/home/reducer';
+import playerDetailsReducer from 'modules/player/reducer';
+import playlistsReducer from 'modules/playlists/reducer';
 import recommendedReducer from 'modules/recommended/reducer';
 import searchResultReducer from 'modules/search_index/reducer';
 import subscriptionsReducer from 'modules/subscriptions/reducer';
 import trendingReducer from 'modules/trending/reducer';
 import userReducer from 'modules/user/reducer';
-import settingReducer from 'common/setting/reducer';
-import playlistsReducer from 'modules/playlists/reducer';
-import playerDetailsReducer from 'modules/player/reducer';
 
 const rootReducer = combineReducers({
-  homeChannels: homeChannelsReducer,
   channelDetails: channelDetailsReducer,
+  homeChannels: homeChannelsReducer,
+  playerDetails: playerDetailsReducer
+  playlists: playlistsReducer,
   query: queryReducer,
   recommended: recommendedReducer,
   searchResult: searchResultReducer,
+  setting: settingReducer,
   subscriptions: subscriptionsReducer,
   trending: trendingReducer,
   user: userReducer,
-  setting: settingReducer,
-  playlists: playlistsReducer,
-  playerDetails: playerDetailsReducer
 });
 
 export default rootReducer;
