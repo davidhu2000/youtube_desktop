faa4bae27cb41144c524709b0600d49ea891b11f
David Hu <davidhu314@gmail.com>
2017-05-01 23:08:13 -0700

    add notes for how player page responsiveness should be built

--- src/modules/player/component.jsx
+++ src/modules/player/component.jsx
@@ -20,9 +20,21 @@ class PlayerDetails extends React.Component {
     super(props);
 
     this.state = {
-      videoId: this.props.router.params.videoId
+      videoId: this.props.router.params.videoId,
+      playerSize: {
+        small: { width: 426, height: 240 },
+        medium: { width: 640, height: 360 },
+        large: { width: 854, height: 480 } 
+      }
     };
   }
+
+  // >1300px large player, related on right
+  // >1000px <1300px, medium player, related on right
+  // >860px <1000px, large player, related on bottom
+  // >660px <860px, medium player, related on bottom
+  // <660px, small player, related on bottom
+
 
   _getNewVideoInfo(videoId) {
     let dataNeeded = [];
